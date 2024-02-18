25bf307bd057c29f47fb12255803ae2cc9e6152a
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-02-18 14:26:33 +0900

    modified:   lean4/Main.lean

--- lean4/Main.lean
+++ lean4/Main.lean
@@ -1,4 +1,19 @@
 import «Le»
 
+-- def onePlusOneIsThree : 1 + 1 = 3 := rfl
+def onePlusOneIsTwo : 1 + 1 = 2 := by
+  simp
+
+theorem add : 1 + 1 = 2 := by
+  simp
+
+theorem append : "Str".append "ing" = "String" := by
+  rfl
+
+theorem andImpliesOr : A ∧ B → A ∨ B :=
+  fun andEvidence =>
+    match andEvidence with
+    | And.intro a _ => Or.inl a
+
 def main : IO Unit :=
   IO.println s!"Hello, {hello}!"
