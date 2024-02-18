986a5e0398bd8364a090f3cfe909fcb8f73fb82d
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-02-18 13:34:19 +0900

    add lean4

---
+++ lean4/Main.lean
@@ -0,0 +1,4 @@
+import «Le»
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ lean4/lakefile.lean
@@ -0,0 +1,12 @@
+import Lake
+open Lake DSL
+
+package «Le» where
+  -- add package configuration options here
+
+lean_lib «Le» where
+  -- add library configuration options here
+
+@[default_target]
+lean_exe «le» where
+  root := `Main
