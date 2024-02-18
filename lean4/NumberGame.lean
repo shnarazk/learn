1fa98242fca07062ea79a84e0c3b92386e8d4783
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-02-19 01:19:51 +0900

    lean4: Number Game ep.2 done

--- lean4/NumberGame.lean
+++ lean4/NumberGame.lean
@@ -23,7 +23,7 @@ def mynat_to_nat (n: MyNat): Nat :=
   match n with
   | MyNat.zero => Nat.zero
   | MyNat.succ n' => Nat.succ (mynat_to_nat n')
-  
+
 instance : OfNat MyNat n where
  ofNat := nat_to_mynat n
 
@@ -55,40 +55,45 @@ example : mul 0 5 = 0 := rfl
 instance : Mul MyNat where
   mul := mul
 
--- lemma addIsCommutive : forall (x y : MyNat), add x y = add y x := sorry
---   induction
-
 lemma example1 (x y z : MyNat) : x * y + z = x * y + z := by
   rfl
 
 lemma example2 (x y : MyNat) (h : y = x + 7) : 2 * y = 2 * (x + 7) := by
   rewrite [h]
   rfl
-  
+
 lemma example3 (a b : MyNat) (h : succ a = b) : succ (succ a) = succ b := by
   rewrite [h]
   rfl
 
-lemma my_add_zero (a : MyNat) : 0 + a = a := by
-  rfl
-  
-lemma add_succ (a b : MyNat) : (succ a) + b  = succ (a + b) := by
+lemma my_add_zero_left (a : MyNat) : zero + a = a := by
   rfl
 
-lemma add_succ_zero (a : MyNat) : (succ 0) + a = succ a := by
-  rewrite [add_succ, my_add_zero]
+lemma my_add_succ_left (a b : MyNat) : succ a + b  = succ (a + b) := by
   rfl
 
-lemma add_zero_right (a : MyNat) : a + 0 = a := by
+lemma my_add_zero_right (a : MyNat) : a + zero = a := by
   induction a with
   | zero => rfl
-  | succ a' ih =>
-      rewrite [add_succ, ih]
-      rfl
+  | succ a' ih => rewrite [my_add_succ_left, ih]; rfl
 
-example : 4 + 0 = 0 + 4 := rfl
+lemma my_add_succ_right (a b : MyNat) : a + succ b  = succ (a + b) := by
+  induction a with
+    | zero => rewrite [my_add_zero_left, my_add_zero_left]; rfl
+    | succ a' ih => rewrite [my_add_succ_left, ih]; rfl
 
--- lemma my_add_assoc (a b c : MyNat) : (a + b) + c = a + (b + c) := by
---   induction c with
---     | zero => sorry
---     | succ c' ih => sorry
+lemma my_add_succ_zero (a : MyNat) : (succ zero) + a = succ a := by
+  rewrite [my_add_succ_left, my_add_zero_left]
+  rfl
+
+lemma my_add_is_commutive (x y : MyNat) : x + y = y + x := by
+  induction x with
+  | zero => rewrite [my_add_zero_left, my_add_zero_right]; rfl
+  | succ x' ih => rewrite [my_add_succ_right, my_add_succ_left, ih]; rfl
+
+lemma my_add_assoc_nqwddg (a b c : MyNat) : (a + b) + c = a + (b + c) := by
+  induction c with
+    | zero => rewrite [my_add_zero_right, my_add_zero_right]; rfl
+    | succ c' ih => rewrite [my_add_succ_right, my_add_succ_right, my_add_succ_right, ih]; rfl
+
+example : 2 + (5 + 8) = (2 + 5) + 8 := rfl
