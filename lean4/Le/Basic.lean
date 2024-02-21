07fd17a19ab284857187b20656405abd282b5277
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-02-21 10:50:12 +0900

    modified:   lean4/Le/Basic.lean

--- lean4/Le/Basic.lean
+++ lean4/Le/Basic.lean
@@ -1,11 +1,11 @@
-import Mathlib.Data.Nat.Basic
+-- import Mathlib.Data.Nat.Basic
 import Lean
 
 def hello := "world"
 
 open Nat
 
-def fib (n : ℕ) : ℕ :=
+def fib (n : Nat) : Nat :=
   match n with
   | zero => 0
   | succ zero => 1
@@ -18,10 +18,10 @@ def fib (n : ℕ) : ℕ :=
 -- #eval fib 4
 -- #eval fib 5
 
-lemma fib_is_fib (n : ℕ) : fib (succ (succ n)) = fib (succ n) + fib n := by
-  rw [fib]
+-- lemma fib_is_fib (n : Nat) : fib (succ (succ n)) = fib (succ n) + fib n := by rw [fib]
 
-def leibniz (n: ℕ) (sum: Float) : Float :=
+-- compute pi
+def leibniz (n : Nat) (sum : Float) : Float :=
   match n with
-  | 0 => sum
-  | succ n' =>  let k := (n * 4).toFloat; leibniz n' (sum + 8.0 / ((k - 1.0) * (k - 3.0)))
+  | 0 => sum + 8.0 / 3.0
+  | succ n' => let k := (n * 4).toFloat; leibniz n' (sum + 8.0 / ((k + 1.0) * (k + 3.0)))
