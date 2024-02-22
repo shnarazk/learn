59c6337257a3ae2bcfe751afe69b40784c6af2ca
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-02-22 12:27:15 +0900

    inject mathematics in Lean

--- lean4/old/lakefile.lean
+++ lean4/old/lakefile.lean
@@ -1,14 +1,19 @@
 import Lake
 open Lake DSL
 
-package mil where
+package «Le» where
+  -- add package configuration options here
   leanOptions := #[
     ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
     ⟨`autoImplicit, false⟩,
     ⟨`relaxedAutoImplicit, false⟩]
 
-@[default_target]
-lean_lib MIL where
+lean_lib «Le» where
+  -- add library configuration options here
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4"@"master"
+@[default_target]
+lean_exe «le» where
+  root := `Main
+
+require mathlib from git "https://github.com/leanprover-community/mathlib4"
 require std from git "https://github.com/leanprover/std4" @ "main"
