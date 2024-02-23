3b18e687c10de6e0b762666a8f3c9eb312a3c63f
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-02-23 22:32:24 +0900

    modified:   lean4/misc/Main.lean

--- lean4/misc/Main.lean
+++ lean4/misc/Main.lean
@@ -5,3 +5,7 @@ def main : IO Unit := do
   IO.println s!"pi = {leibniz 10000000} ."
   let lines ← readData "lakefile.lean"
   lines.forM IO.println
+  let x ← IO.getEnv "HOME"
+  match x with
+  | some x => IO.println x
+  | none => pure ()
