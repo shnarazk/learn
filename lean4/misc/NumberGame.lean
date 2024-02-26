e7035ebb27b60aa4ad31cf8e30af537521bd1ae3
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-02-26 22:10:27 +0900

    modified:   misc/NumberGame.lean

--- lean4/misc/NumberGame.lean
+++ lean4/misc/NumberGame.lean
@@ -13,19 +13,18 @@ inductive MyNat where
 #eval MyNat.succ MyNat.zero
 
 open MyNat
-
-def nat_to_mynat (n: Nat): MyNat :=
+def nat_to_mynat (n : Nat) : MyNat :=
   match n with
   | Nat.zero => MyNat.zero
   | Nat.succ n' => MyNat.succ (nat_to_mynat n')
+
+instance (n : Nat) : OfNat MyNat (n) where
+  ofNat := nat_to_mynat n
 
 def mynat_to_nat (n: MyNat): Nat :=
   match n with
   | MyNat.zero => Nat.zero
   | MyNat.succ n' => Nat.succ (mynat_to_nat n')
-
-instance : OfNat MyNat n where
- ofNat := nat_to_mynat n
 
 #eval succ zero
 
@@ -38,6 +37,7 @@ instance : Add MyNat where
   add := add
 
 #eval add (succ (succ zero)) (succ zero)
+#eval add 2 1
 #eval add (nat_to_mynat 7) (nat_to_mynat 3)
 
 example : add 7 3 = 10 := rfl
