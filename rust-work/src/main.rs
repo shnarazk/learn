a4501f9d074429194994c043c6c7141374b51d38
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-03-25 12:35:13 +0900

    modified:   rust-work/src/main.rs

--- rust-work/src/main.rs
+++ rust-work/src/main.rs
@@ -1,14 +1,26 @@
+const LIMIT: usize = 10_000_000;
 #[tokio::main]
 async fn main() -> Result<(), ()> {
     let dict = build_dict();
     println!("Hello, world: {:?}!", &dict[6400..6580]);
-    let v = build_sequence();
-    println!("Hello, world: {:?}!", &v[6400..6580]);
+    let values = build_sequence();
+    println!("Hello, world: {:?}!", &values[6400..6580]);
+    let x: isize = values
+        .iter()
+        .map(|x| {
+            if let Some((i, _)) = dict.iter().enumerate().find(|entry| entry.1 == x) {
+                i as isize
+            } else {
+                -1_isize
+            }
+        })
+        .sum();
+    println!("{x}");
     Ok(())
 }
 
 fn build_sequence() -> Vec<usize> {
-    let mut vec = vec![0; 100000];
+    let mut vec = vec![0; LIMIT];
     for (i, p) in vec.iter_mut().enumerate() {
         *p = (usize::next_power_of_two(i)) % ((usize::count_ones(i) + 3) as usize)
             * ((i * i) % (usize::count_zeros(i) as usize / 2));
@@ -17,7 +29,7 @@ fn build_sequence() -> Vec<usize> {
 }
 
 fn build_dict() -> Vec<usize> {
-    let mut vec = vec![0; 100000];
+    let mut vec = vec![0; LIMIT / 10];
     let mut pre = 0;
     for (i, p) in vec.iter_mut().enumerate() {
         *p = (usize::next_power_of_two(i)) % ((usize::count_ones(i + 3) + 1) as usize)
