7f6415ee990b28f74a4013aa54a4e47fddaf4c24
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-05-09 20:14:39 +0900

    error handling

--- on_ratatui/src/main.rs
+++ on_ratatui/src/main.rs
@@ -1,4 +1,8 @@
 use {
+    color_eyre::{
+        eyre::{bail, WrapErr},
+        Result,
+    },
     crossterm::{
         event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
         terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
@@ -9,7 +13,7 @@ use {
         symbols::border,
         widgets::{block::*, *},
     },
-    std::io::{self, stdout, Result},
+    std::io::{self, stdout},
 };
 mod errors;
 mod tui;
@@ -21,41 +25,49 @@ pub struct App {
 }
 
 impl App {
-    pub fn run(&mut self, terminal: &mut tui::Tui) -> io::Result<()> {
+    pub fn run(&mut self, terminal: &mut tui::Tui) -> Result<()> {
         while !self.exit {
             terminal.draw(|frame| self.render_frame(frame))?;
-            self.handle_events()?;
+            self.handle_events().wrap_err("handle events failed")?;
         }
         Ok(())
     }
     fn render_frame(&self, frame: &mut Frame) {
         frame.render_widget(self, frame.size());
     }
-    fn handle_events(&mut self) -> io::Result<()> {
+    fn handle_events(&mut self) -> Result<()> {
         match event::read()? {
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                self.handle_key_event(key_event)
-            }
-            _ => (),
-        };
-        Ok(())
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => self
+                .handle_key_event(key_event)
+                .wrap_err_with(|| format!("handling key event failed:\n{key_event:#?}")),
+            _ => Ok(()),
+        }
     }
-    fn handle_key_event(&mut self, key_event: KeyEvent) {
+    fn handle_key_event(&mut self, key_event: KeyEvent) -> Result<()> {
         match key_event.code {
             KeyCode::Char('q') => self.exit(),
-            KeyCode::Left => self.decrement_counter(),
-            KeyCode::Right => self.increment_counter(),
+            KeyCode::Left => self.decrement_counter()?,
+            KeyCode::Right => self.increment_counter()?,
             _ => (),
         }
+        Ok(())
     }
     fn exit(&mut self) {
         self.exit = true;
     }
-    fn increment_counter(&mut self) {
-        self.counter += 1;
-    }
-    fn decrement_counter(&mut self) {
+    fn decrement_counter(&mut self) -> Result<()> {
+        if self.counter == 0 {
+            bail!("counter underflow");
+        }
         self.counter -= 1;
+        Ok(())
+    }
+    fn increment_counter(&mut self) -> Result<()> {
+        self.counter += 1;
+        if 2 < self.counter {
+            bail!("counter overflow");
+        }
+        Ok(())
     }
 }
 
@@ -96,9 +108,9 @@ impl Widget for &App {
 fn main() -> Result<()> {
     errors::install_hooks()?;
     let mut terminal = tui::init()?;
-    let app_result = App::default().run(&mut terminal);
-    let _ = tui::restore();
-    app_result
+    App::default().run(&mut terminal)?;
+    tui::restore()?;
+    Ok(())
 }
 
 #[allow(dead_code)]
@@ -166,16 +178,35 @@ mod tests {
     #[test]
     fn handle_key_event() -> io::Result<()> {
         let mut app = App::default();
-        app.handle_key_event(KeyCode::Right.into());
+        app.handle_key_event(KeyCode::Right.into()).unwrap();
         assert_eq!(app.counter, 1);
 
-        app.handle_key_event(KeyCode::Left.into());
+        app.handle_key_event(KeyCode::Left.into()).unwrap();
         assert_eq!(app.counter, 0);
 
         let mut app = App::default();
-        app.handle_key_event(KeyCode::Char('q').into());
+        app.handle_key_event(KeyCode::Char('q').into()).unwrap();
         assert_eq!(app.exit, true);
 
         Ok(())
+    }
+    #[test]
+    // #[should_panic(expected = "attempt to subtract with underflow")]
+    #[should_panic(expected = "counter underflow")]
+    fn handle_key_event_panic() {
+        let mut app = App::default();
+        app.handle_key_event(KeyCode::Left.into()).unwrap();
+    }
+    #[test]
+    fn handle_key_event_overflow() {
+        let mut app = App::default();
+        assert!(app.handle_key_event(KeyCode::Right.into()).is_ok());
+        assert!(app.handle_key_event(KeyCode::Right.into()).is_ok());
+        assert_eq!(
+            app.handle_key_event(KeyCode::Right.into())
+                .unwrap_err()
+                .to_string(),
+            "counter overflow"
+        );
     }
 }
