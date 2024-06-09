df8b9a1a11b1cf445d9219352f009b8474066f2b
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-06-09 09:35:59 +0900

    Switch to 4.9.0-rc1 -	modified:   MIL/C08_Groups_and_Rings/S01_Groups.lean -	modified:   lake-manifest.json -	modified:   lean-toolchain

--- mathematics_in_lean/MIL/C08_Groups_and_Rings/S01_Groups.lean
+++ mathematics_in_lean/MIL/C08_Groups_and_Rings/S01_Groups.lean
@@ -274,7 +274,7 @@ open MonoidHom
 #check Nat.eq_of_mul_eq_mul_right
 
 -- The following line is working around a Lean bug that will be fixed very soon.
-attribute [-instance] Subtype.instInhabited
+-- attribute [-instance] Subtype.instInhabited
 
 lemma aux_card_eq [Fintype G] (h' : card G = card H * card K) : card (G ⧸ H) = card K := by
   sorry
