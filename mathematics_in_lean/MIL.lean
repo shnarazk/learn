f4afd11fdc785cc4e4684d0bc4f294c4d84a31fa
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-06-27 20:54:47 +0900

    switch to 4.3

--- mathematics_in_lean/MIL.lean
+++ mathematics_in_lean/MIL.lean
@@ -12,8 +12,8 @@
 -- import MIL.C03_Logic.S05_Disjunction
 -- import MIL.C03_Logic.S06_Sequences_and_Convergence
 -- import MIL.C04_Sets_and_Functions.S01_Sets
-import MIL.C04_Sets_and_Functions.S02_Functions
--- import MIL.C04_Sets_and_Functions.S03_The_Schroeder_Bernstein_Theorem
+-- import MIL.C04_Sets_and_Functions.S02_Functions
+import MIL.C04_Sets_and_Functions.S03_The_Schroeder_Bernstein_Theorem
 -- import MIL.C05_Elementary_Number_Theory.S01_Irrational_Roots
 -- import MIL.C05_Elementary_Number_Theory.S02_Induction_and_Recursion
 -- import MIL.C05_Elementary_Number_Theory.S03_Infinitely_Many_Primes
