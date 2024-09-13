fa50daf464c035f437cf3b387c25fe9e88dddd28
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-09-13 16:56:13 +0900

    (mil) switch to C08S01

--- mathematics_in_lean/MIL.lean
+++ mathematics_in_lean/MIL.lean
@@ -22,8 +22,8 @@
 -- import MIL.C06_Structures.S03_Building_the_Gaussian_Integers
 -- import MIL.C07_Hierarchies.S01_Basics
 -- import MIL.C07_Hierarchies.S02_Morphisms
-import MIL.C07_Hierarchies.S03_Subobjects
--- import MIL.C08_Groups_and_Rings.S01_Groups
+-- import MIL.C07_Hierarchies.S03_Subobjects
+import MIL.C08_Groups_and_Rings.S01_Groups
 -- import MIL.C08_Groups_and_Rings.S02_Rings
 -- import MIL.C09_Topology.S01_Filters
 -- import MIL.C09_Topology.S02_Metric_Spaces
