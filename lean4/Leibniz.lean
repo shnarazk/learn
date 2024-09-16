a3347db747dd9e2540c73c5c55cf5cc0d6bd847d
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-09-16 20:15:59 +0900

    (lean4) new file:   Leibniz.lean

---
+++ lean4/Leibniz.lean
@@ -0,0 +1,19 @@
+import Mathlib.Data.Nat.Defs
+
+open Nat
+
+def fib (n : ℕ) : ℕ :=
+  match n with
+  | zero => 0
+  | succ zero => 1
+  | succ (succ n₂) => fib (n₂ + 1) + fib n₂
+
+#eval fib 0
+#eval fib 1
+#eval fib 2
+#eval fib 3
+#eval fib 4
+#eval fib 5
+
+lemma fib_is_fib (n : ℕ) : fib (succ (succ n)) = fib (succ n) + fib n := by
+  rw [fib]
