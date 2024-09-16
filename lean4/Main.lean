749e8fb4c126245de6e0caa0c12a6f6fa585d249
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-09-16 20:32:52 +0900

    (lean4) modified for benchmarking:   Main.lean

--- lean4/Main.lean
+++ lean4/Main.lean
@@ -1,17 +1,19 @@
 import «Le»
-import «Combinator»
+-- import «Combinator»
 -- import Mathlib.Data.Nat.Basic
 import Mathlib.Data.Real.Basic
 
 def main : IO Unit := do
-  IO.println s!"fib 20 = {fib 20} ."
+  -- IO.println s!"fib 20 = {fib 20} ."
   IO.println s!"pi = {leibniz 10000000} ."
+  /-
   let lines ← readData "lakefile.lean"
   lines.forM IO.println
   let x ← IO.getEnv "HOME"
   match x with
   | some x => IO.println x
   | none => pure ()
+  -/
 
 #eval Nat.gcd 20 5
 #eval Nat.lcm 20 5
