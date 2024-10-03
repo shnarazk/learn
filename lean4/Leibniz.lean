66d1fa9322c168ac56c7940b5b1de1b4412653b9
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-10-03 22:15:02 +0900

    (lean4) start the proof

--- lean4/Leibniz.lean
+++ lean4/Leibniz.lean
@@ -1,3 +1,4 @@
+import Mathlib.Tactic
 open Nat
 
 -- compute pi
@@ -20,3 +21,22 @@ def leibnizIO (n : Nat) : IO Float := do return leibniz n
 
 #eval leibniz 1000
 
+/-
+-- This is the FUN part only on Lean4
+-/
+
+namespace this_is_pi_approximation
+
+def leibniz (n : Nat) (k: Float) (sum : Float) : Float :=
+  match n with
+  | zero => sum + 8 / 3
+  | succ n' => leibniz₁ n' (k - 4) (sum + 8 / ((k + 1) * (k + 3)))
+
+def leibniz4 (n : Nat) (k: Float) (sum : Float) : Float :=
+  match n with
+  | zero => sum + 1/1 - 1 / 3
+  | succ n' => leibniz₁ n' (k - 4) (sum + 1 / (k + 1) - 1 / (k + 3))
+
+lemma leibniz4_mul_4_equals_to_leibniz : ∀ n : Nat,
+    4 * leibniz4 n (4 * n.toFloat) 0 = leibniz n (4 * n.toFloat) 0 := by
+  sorry
