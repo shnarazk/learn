7855627415ea398f186cecd446bf5de99f91a2a1
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-01-04 21:19:55 +0900

    (lean4) once again switch to Lean-4.15.0

--- lean4/Main.lean
+++ lean4/Main.lean
@@ -1,18 +1,18 @@
--- import Aesop
--- import Mathlib.Data.Real.Basic
+import Aesop
+import Mathlib.Data.Real.Basic
 -- import Mathlib.Data.Nat.Basic
--- import Leibniz
+import Leibniz
 -- import «Le»
 -- import «Combinator»
 
 def main : IO Unit := do
   let pairs : Nat := 10 * 1000 * 1000
-  -- let (result, time) ← Aesop.time <| leibnizIO pairs
-  -- IO.println s!"pi {pairs} = {result} in {time.printAsMillis}."
+  let (result, time) ← Aesop.time <| leibnizIO pairs
+  IO.println s!"pi {pairs} = {result} in {time.printAsMillis}."
 
   let pairs : Nat := 100 * 1000 * 1000
-  -- let (result, time) ← Aesop.time <| leibnizIO pairs
-  -- IO.println s!"pi {pairs} = {result} in {time.printAsMillis}."
+  let (result, time) ← Aesop.time <| leibnizIO pairs
+  IO.println s!"pi {pairs} = {result} in {time.printAsMillis}."
   /-
   let lines ← readData "lakefile.lean"
   lines.forM IO.println
