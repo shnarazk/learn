acbf4297bfddad3be67bb96cec25faf0de148c4d
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-04-15 21:17:02 +0900

    Replace Aesop.time with custom timing function
    Update Lean from v4.19.0-rc2 to v4.19.0-rc3 and remove direct dependency
    on Aesop.

--- lean4/Main.lean
+++ lean4/Main.lean
@@ -1,18 +1,27 @@
-import Aesop
 import Mathlib.Data.Real.Basic
 -- import Mathlib.Data.Nat.Basic
 import Math.Leibniz
+-- import Lean
 -- import «Le»
 -- import «Combinator»
 
+universe u
+
+@[inline]
+def elapsedTime {α : Type} {m : Type → Type u} [Monad m] [MonadLiftT BaseIO m] (x : m α) : m (α × Nat) := do
+  let beg ← IO.monoNanosNow
+  let val ← x
+  let fin ← IO.monoNanosNow
+  return (val, fin - beg)
+
 def main : IO Unit := do
   let pairs : Nat := 10 * 1000 * 1000
-  let (result, time) ← Aesop.time <| leibnizIO pairs
-  IO.println s!"pi {pairs} = {result} in {time.printAsMillis}."
+  let (result, time) ← elapsedTime <| leibnizIO pairs
+  IO.println s!"pi {pairs} = {result} in {time / 1000_000} msec."
 
   let pairs : Nat := 100 * 1000 * 1000
-  let (result, time) ← Aesop.time <| leibnizIO pairs
-  IO.println s!"pi {pairs} = {result} in {time.printAsMillis}."
+  let (result, time) ← elapsedTime <| leibnizIO pairs
+  IO.println s!"pi {pairs} = {result} in {time / 1000_000} msec."
   /-
   let lines ← readData "lakefile.lean"
   lines.forM IO.println
@@ -25,4 +34,3 @@ def main : IO Unit := do
 -- #eval Nat.gcd 20 5
 -- #eval Nat.lcm 20 5
 -- theorem ConcrateMath (n m : Nat) : (n % m) = max (Nat.gcd n m) (Nat.gcd m n) := sorry
-
