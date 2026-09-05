ec5559bfdcda3b3961b0a0e45e972b4368850e24
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-09-05 21:52:25 +0900

    Lean4: kill mathlib4 dependency

--- Lean4/Math.lean
+++ Lean4/Math.lean
@@ -1,13 +1,1 @@
 module
-
-public import Math.Fib
-public import Math.Leibniz
-public import Math.LinearAlgebra
-public import Math.Luby
-public import Math.Unitary
-
-/-!
-texmath使える？
-
-$`\log(N)`
--/
